c96bfb617e66a4c77b6b0756a5dea69f35145b11
vthriller <unixway.drive+git@gmail.com>
2016-02-07 04:41:13 +0300

    trying to save even more by encoding spaces and digits and whatnot with one byte

--- utfp.hs
+++ utfp.hs
@@ -7,29 +7,36 @@ import Data.Word
 
 main = do
 	input <- B.getContents
-	B.putStr . B.pack . (map fromIntegral) . (encode 0 0 0) . (map ord) . U.decode . B.unpack $ input
+	B.putStr . B.pack . (map fromIntegral) . (encode 1 0 0) . (map ord) . U.decode . B.unpack $ input
 	where
 		{-
-		'0xxx xxxx' to represent '(p2)(p1)(p0)xxx xxxx' char
-		'10xx xxxx' to set p0 to 'xx xxxx' (initially set to 0)
+		'00xx xxxx' to represent 'xx xxxx' char
+			used to avoid prefix setters for commonly used whitespace characters, digits, and punctuation
+		'01xx xxxx' to represent '(p2)(p1)(p0)xx xxxx' char
+		'10xx xxxx' to set p0 to 'xx xxxx' (initially set to 1)
+			this initial value makes ASCII latin renderable in absence of '1000 0001' prefix setter
 		'110x xxxx' to set p1 to 'x xxxx' (initially set to 0)
 		'111x xxxx' to set p2 to 'x xxxx' (initially set to 0)
 
-		This covers the range of 0x00..0x7fffff
+		This covers the range of 0x00..0x3fffff,
+		which happens to cover all valid Unicode code points (U+0000 to U+10FFFF)
 		-}
 		-- ppN is for currently used prefix bits
-		encode :: (Eq a, Num a, Bits a) => a -> a -> a -> [a] -> [a]
+		encode :: (Eq a, Ord a, Num a, Bits a) => a -> a -> a -> [a] -> [a]
 		encode pp0 pp1 pp2 (c:cs) =
+			if c < 0b1000000
+			then c : encode pp0 pp1 pp2 cs
+			else
 			( if pp2 == p2 then id else (p2' :) ) $
 			( if pp1 == p1 then id else (p1' :) ) $
 			( if pp0 == p0 then id else (p0' :) ) $
 			b : encode p0 p1 p2 cs
 			where
-				p0 = (c `shiftR` 7)         .&. 0b111111
-				p1 = (c `shiftR` (7+6))     .&. 0b11111
-				p2 = (c `shiftR` (7+6+5))   .&. 0b11111
+				p0 = (c `shiftR` 6)         .&. 0b111111
+				p1 = (c `shiftR` (6+6))     .&. 0b11111
+				p2 = (c `shiftR` (6+6+5))   .&. 0b11111
 				p0' = p0 .|. 0b10000000
 				p1' = p1 .|. 0b11000000
 				p2' = p2 .|. 0b11100000
-				b = c .&. 0b1111111
+				b = (c .&. 0b111111) .|. 0b01000000
 		encode _ _ _ [] = []
